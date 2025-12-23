a32956b4526907361e980d9c9861f8eb23679b2e
BryanB-prog <neverthoughttoquestionwhy@gmail.com>
2025-12-23 09:59:58 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ const pillars: Pillar[] = [
       </ul>
       
     ),
-    bg: "bg-[url('/pillar1.png')] bg-cover bg-center bg-no-repeat bg-opacity-20"
+    bg: ""
     href: "/",
     points: ["R (tidyverse, ggplot2)", "Python (pandas, NumPy, SciPy)"],
   },
