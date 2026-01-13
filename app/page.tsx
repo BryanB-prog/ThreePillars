6252b074de7c7ed9e4a60cf8a0edfb136ba12e14
BryanB-prog <neverthoughttoquestionwhy@gmail.com>
2026-01-13 17:17:01 -0500

    Refactor HomePage component layout and styles

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Card } from "@/components/Card";
 
 const HomePage: React.FC = () => {
   return (
-    <div className="max-h-screen flex flex-col items-center justify-start p-8">
+    <div className="max-h-screen flex flex-col items-center justify-start p-12">
       <section className="p-6 rounded-lg shadow-lg max-w-3xl w-full">
         <p className="text-gray-500">
           Hi, I’m Bryan. I’m a data scientist with an experienced background in
@@ -232,4 +232,5 @@ const HomePage: React.FC = () => {
 };
 
 export default HomePage;
+
 
